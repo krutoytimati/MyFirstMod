91a5ea3b2c180f1474ae620cef00550d5681716e
Forohon <timur16.06@mail.ru>
2023-07-11 00:44:58 +0400

    update

--- src/main/java/com/integral/examplemod/items/ring.java
+++ src/main/java/com/integral/examplemod/items/ring.java
@@ -1,10 +1,12 @@
 package com.integral.examplemod.items;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 public class ring extends Item {
 
     public ring() {
         this.setRegistryName("ring");
-        this.setTranslationKey("ghastTear");
+        this.setTranslationKey("examplemod.ring");
+        this.setCreativeTab(CreativeTabs.FOOD);
     }
 }
